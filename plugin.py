e42a9af96610ea6fe24c52798e4eb3bf17b73dce
Sjaak Spiegels <sjaak@Sjaak-MacBook-Pro.local>
2019-12-31 13:41:52 +0100

    plugin name

--- plugin.py
+++ plugin.py
@@ -3,7 +3,7 @@
 # Author: Sjaak Spiegels
 #
 """
-<plugin key="Garagedoor" name="Garagedoor" version="1.0.0" author="Sjaak" wikilink="" externallink="">
+<plugin key="Ventilatie-systeem" name="Garagedoor" version="1.0.0" author="Sjaak" wikilink="" externallink="">
     <params>
         <param field="Address" label="MQTT Server" width="200px" required="true" default=""/>
         <param field="Port" label="MQTT Port" width="150px" required="true" default="1883"/>
